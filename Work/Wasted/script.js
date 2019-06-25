c6c96babe7bad0f39c0c72c928711057d6fad8dd
FifiXie <41806279+FifiXie@users.noreply.github.com>
2019-06-25 16:13:30 -0400

    update

--- Work/Wasted/script.js
+++ Work/Wasted/script.js
@@ -15,12 +15,28 @@ function topFunction() {
 }
 
 
-// List of sentences
-// var OO = &#334; ;
-// console.log (OO);
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+// List of sentences
 var _CONTENT = [ 
-	"  Mo   "
+	"Wasted "
 ];
 
 // Current sentence being processed
