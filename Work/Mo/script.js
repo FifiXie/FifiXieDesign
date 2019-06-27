fe0f88840592a2b3629fcb6c82b92f1a882ffe7a
FifiXie <41806279+FifiXie@users.noreply.github.com>
2019-06-27 11:45:08 -0400

    mo

--- Work/Mo/script.js
+++ Work/Mo/script.js
@@ -86,3 +86,20 @@ function Delete() {
 
 // Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 100);
+
+
+
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 1000); 
+// }
